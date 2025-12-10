52aec3574c0efba7053f354099caad5b8a816b27
JUAN <eloicx@gmail.com>
2025-12-10 15:17:08 -0600

    PROYECTO 1
    Test kernel 01

---
+++ modulo-kernel/sysinfo.mod.c
@@ -0,0 +1,93 @@
+#include <linux/module.h>
+#include <linux/export-internal.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x003b23f9, "single_open" },
+	{ 0x33c78c8a, "remove_proc_entry" },
+	{ 0xc7ffe1aa, "si_meminfo" },
+	{ 0xf2c4f3f1, "seq_printf" },
+	{ 0xd272d446, "__rcu_read_lock" },
+	{ 0x1790826a, "init_task" },
+	{ 0xe8d8d116, "get_task_mm" },
+	{ 0x397daafe, "mmput" },
+	{ 0xd272d446, "__rcu_read_unlock" },
+	{ 0x04e8afba, "access_process_vm" },
+	{ 0x90a48d82, "__ubsan_handle_out_of_bounds" },
+	{ 0xd272d446, "__stack_chk_fail" },
+	{ 0xbd4e501f, "seq_read" },
+	{ 0xfc8fa4ce, "seq_lseek" },
+	{ 0xcb077514, "single_release" },
+	{ 0xd272d446, "__fentry__" },
+	{ 0x82c6f73b, "proc_create" },
+	{ 0xe8213e80, "_printk" },
+	{ 0xd272d446, "__x86_return_thunk" },
+	{ 0xba157484, "module_layout" },
+};
+
+static const u32 ____version_ext_crcs[]
+__used __section("__version_ext_crcs") = {
+	0x003b23f9,
+	0x33c78c8a,
+	0xc7ffe1aa,
+	0xf2c4f3f1,
+	0xd272d446,
+	0x1790826a,
+	0xe8d8d116,
+	0x397daafe,
+	0xd272d446,
+	0x04e8afba,
+	0x90a48d82,
+	0xd272d446,
+	0xbd4e501f,
+	0xfc8fa4ce,
+	0xcb077514,
+	0xd272d446,
+	0x82c6f73b,
+	0xe8213e80,
+	0xd272d446,
+	0xba157484,
+};
+static const char ____version_ext_names[]
+__used __section("__version_ext_names") =
+	"single_open\0"
+	"remove_proc_entry\0"
+	"si_meminfo\0"
+	"seq_printf\0"
+	"__rcu_read_lock\0"
+	"init_task\0"
+	"get_task_mm\0"
+	"mmput\0"
+	"__rcu_read_unlock\0"
+	"access_process_vm\0"
+	"__ubsan_handle_out_of_bounds\0"
+	"__stack_chk_fail\0"
+	"seq_read\0"
+	"seq_lseek\0"
+	"single_release\0"
+	"__fentry__\0"
+	"proc_create\0"
+	"_printk\0"
+	"__x86_return_thunk\0"
+	"module_layout\0"
+;
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "BF72B6D54CD8E4D7D2635EF");
