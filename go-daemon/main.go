bae380e39d8abef462b1f8288a733e7af333601c
choc1403 <eloicx@gmail.com>
2025-12-11 15:13:45 -0600

    PROYECTO 1
    Pruebas para inicializar sqlite

--- go-daemon/main.go
+++ go-daemon/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
+	"so1-daemon/database"
 	"so1-daemon/utils"
+	"so1-daemon/var_const"
 )
 
 func main() {
 
-	// Generar las 3 imagenes
-	if err := utils.BuildImages(); err != nil {
-		log.Printf("Warning: load images failed: %v", err)
+	// Inicializar sqlite
+	if err := database.InitDB(); err != nil {
+		log.Fatalf("DB init error: %v", err)
+	}
+
+	log.Println("DB initialized at", var_const.DB_PATH)
+	if _, err := os.Stat(utils.ABSPATH("./database/data")); os.IsNotExist(err) {
+		_ = os.MkdirAll("./database/data", 0755)
 	}
 
 	// Generar los 10 contenedores
