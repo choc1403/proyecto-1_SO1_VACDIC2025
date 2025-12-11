9e5535777bb119b8f0908b14a2cc0ff94fc8b4b5
choc1403 <eloicx@gmail.com>
2025-12-11 14:23:56 -0600

    PROYECTO 1
    Ejecutar el CRON y probando cargar los modulos del kernel

--- go-daemon/main.go
+++ go-daemon/main.go
@@ -13,8 +13,13 @@ func main() {
 	}
 
 	// Generar los 10 contenedores
-	if err := utils.BuildContainers(); err != nil {
-		log.Printf("Warning: load images failed: %v", err)
+	if err := utils.CreateCron(); err != nil {
+		log.Printf("Warning: create cron failed: %v", err)
+	}
+
+	// Cargar Modulos del Kernel
+	if err := utils.LoadModules(); err != nil {
+		log.Printf("Warning: load modules failed: %v", err)
 	}
 
 }
