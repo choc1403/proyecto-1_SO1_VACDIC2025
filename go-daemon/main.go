a5abaa040bbbf486dfd80b6e361933a66942da21
choc1403 <eloicx@gmail.com>
2025-12-12 11:58:32 -0600

    PROYECTO 1
    Grafana

--- go-daemon/main.go
+++ go-daemon/main.go
@@ -2,67 +2,72 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
-	"so1-daemon/database"
-	"so1-daemon/functions"
 	"so1-daemon/utils"
-	"so1-daemon/var_const"
-	"syscall"
-	"time"
 )
 
 func main() {
+	// basic logger
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("Daemon starting...")
 
-	// Inicializar sqlite
-	if err := database.InitDB(); err != nil {
-		log.Fatalf("DB init error: %v", err)
+	// 1) start grafana
+	if err := utils.StartGrafana(); err != nil {
+		log.Printf("Warning: starting grafana failed: %v", err)
+	} else {
+		log.Println("Grafana started.")
 	}
+	/*
 
-	log.Println("DB initialized at", var_const.DB_PATH)
-	if _, err := os.Stat(utils.ABSPATH("./data")); os.IsNotExist(err) {
-		_ = os.MkdirAll("./data", 0755)
-	}
-
-	// Generar los 10 contenedores
-	if err := utils.CreateCron(); err != nil {
-		log.Printf("Warning: create cron failed: %v", err)
-	}
-
-	// Cargar Modulos del Kernel
-	if err := utils.LoadModules(); err != nil {
-		log.Printf("Warning: load modules failed: %v", err)
-	}
-
-	// handle signals for graceful shutdown
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-
-	// main loop ticker
-	ticker := time.NewTicker(20 * time.Second)
-	defer ticker.Stop()
-
-	if err := functions.ProcessOnce(); err != nil {
-		log.Printf("Initial processOnce error: %v", err)
-	}
-
-loop:
-	for {
-		select {
-		case <-ticker.C:
-			if err := functions.ProcessOnce(); err != nil {
-				log.Printf("processOnce error: %v", err)
+			// Inicializar sqlite
+			if err := database.InitDB(); err != nil {
+				log.Fatalf("DB init error: %v", err)
 			}
-		case <-stop:
-			log.Println("Received stop signal, cleaning up...")
-			break loop
-		}
-	}
 
-	// cleanup
-	if err := utils.RemoveCron(); err != nil {
-		log.Printf("Warning removing cron: %v", err)
-	}
-	log.Println("Daemon exiting.")
+			log.Println("DB initialized at", var_const.DB_PATH)
+			if _, err := os.Stat(utils.ABSPATH("./data")); os.IsNotExist(err) {
+				_ = os.MkdirAll("./data", 0755)
+			}
+
+			// Generar los 10 contenedores
+			if err := utils.CreateCron(); err != nil {
+				log.Printf("Warning: create cron failed: %v", err)
+			}
+
+			// Cargar Modulos del Kernel
+			if err := utils.LoadModules(); err != nil {
+				log.Printf("Warning: load modules failed: %v", err)
+			}
+
+			// handle signals for graceful shutdown
+			stop := make(chan os.Signal, 1)
+			signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+			// main loop ticker
+			ticker := time.NewTicker(20 * time.Second)
+			defer ticker.Stop()
+
+			if err := functions.ProcessOnce(); err != nil {
+				log.Printf("Initial processOnce error: %v", err)
+			}
+
+		loop:
+			for {
+				select {
+				case <-ticker.C:
+					if err := functions.ProcessOnce(); err != nil {
+						log.Printf("processOnce error: %v", err)
+					}
+				case <-stop:
+					log.Println("Received stop signal, cleaning up...")
+					break loop
+				}
+			}
+
+			// cleanup
+			if err := utils.RemoveCron(); err != nil {
+				log.Printf("Warning removing cron: %v", err)
+			}
+			log.Println("Daemon exiting.")
+	*/
 
 }
