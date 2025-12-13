b8768d4ee7848cd781219333489244ea549af706
choc1403 <eloicx@gmail.com>
2025-12-13 00:45:13 -0600

    PROYECTO 1
    Grafana

--- go-daemon/var_const/const.go
+++ go-daemon/var_const/const.go
@@ -15,8 +15,8 @@ const (
 	DOCKER_COMPOSE_F = "docker-compose.yml"
 
 	// Umbrales
-	CPU_THRESHOLD = 15.0 // %
-	MEM_THRESHOLD = 10.0 // %
+	CPU_THRESHOLD = 1.0 // %
+	MEM_THRESHOLD = 1.0 // %
 	// Mínimos
 	MIN_LOW_CONTAINERS  = 3
 	MIN_HIGH_CONTAINERS = 2
