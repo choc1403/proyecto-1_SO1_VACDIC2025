0484a01b0c0c504875620833404a957e76c36d38
choc1403 <eloicx@gmail.com>
2025-12-13 01:17:39 -0600

    PROYECTO 1
    Grafana

--- go-daemon/functions/logic.go
+++ go-daemon/functions/logic.go
@@ -108,6 +108,7 @@ func DecideAndAct(containers []var_const.ProcProcess) {
 		}
 		cpuPct := CalcCpuPercent(c.Proc.Pid, procTime, totalJiffies, now)
 		candidates = append(candidates, decisionCandidate{C: c, Mem: memf, Cpu: cpuPct})
+		log.Println("MEMORIA: ", memf)
 
 		// Save record in DB
 		//log.Println("Guardando en la base de datos...")
