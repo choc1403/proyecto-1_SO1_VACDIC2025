9923c114c51b7273e0f4113d590bc24c3b81ae6b
JUAN <eloicx@gmail.com>
2025-12-13 22:39:58 -0600

    PROYECTO 1

---
+++ modulo-kernel/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_modules.order := {   echo sysinfo.o;   echo continfo.o; :; } > modules.order
---
+++ modulo-kernel/.sysinfo.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_sysinfo.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-headers-6.14.0-37-generic/scripts/module.lds -o sysinfo.ko sysinfo.o sysinfo.mod.o .module-common.o
