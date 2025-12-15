09c61690342f1287763e78d29c0160cc71c37b0e
JUAN <eloicx@gmail.com>
2025-12-14 19:36:09 -0600

    PROYECTO 1

--- bash/detener_cron.sh
+++ bash/detener_cron.sh
@@ -2,7 +2,7 @@
 CRONFILE="/etc/cron.d/project_containers_so1"
 if [ -f "$CRONFILE" ]; then
     rm -f "$CRONFILE"
-    service cron reload || systemctl restart cron || true
+    #service cron reload || systemctl restart cron || true
     echo "cron removed"
 else
     echo "cron not found"
